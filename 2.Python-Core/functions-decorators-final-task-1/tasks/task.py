a849c59c5d58dd5d7cf3925af768685334b710ac
Дмитрий Колб <dm.kolb@gmail.com>
2023-09-12 13:59:57 +0000

    T1 - added asserts for testing

--- 2.Python-Core/functions-decorators-final-task-1/tasks/task.py
+++ 2.Python-Core/functions-decorators-final-task-1/tasks/task.py
@@ -5,3 +5,14 @@ def split(data: str, sep=None, maxsplit=-1):
     Add your code here or call it from here   
     """
     pass
+
+if __name__ == '__main__':
+    assert split('') == []
+    assert split(',123,', sep=',') == ['', '123', '']
+    assert split('test') == ['test']
+    assert split('Python    2     3', maxsplit=1) == ['Python', '2     3']
+    assert split('    test     6    7', maxsplit=1) == ['test', '6    7']
+    assert split('    Hi     8    9', maxsplit=0) == ['Hi     8    9']
+    assert split('    set   3     4') == ['set', '3', '4']
+    assert split('set;:23', sep=';:', maxsplit=0) == ['set;:23']
+    assert split('set;:;:23', sep=';:', maxsplit=2) == ['set', '', '23']
