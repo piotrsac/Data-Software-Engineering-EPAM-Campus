ee8b7e2ce5935732179a5c4f650f1298bf04c27e
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-23 22:54:04 +0100

    commiting solution to task 2

--- 2.Python-Core/data-types-lists-task-2/tasks/task.py
+++ 2.Python-Core/data-types-lists-task-2/tasks/task.py
@@ -2,7 +2,19 @@ from typing import Union, List
 
 ListType = List[Union[int, str]]
 
-
 def get_fizzbuzz_list(n: int) -> ListType:
-    # TODO: Add your code here
-    return []
+    t=[]
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            t.append('FizzBuzz')
+        elif i % 3 == 0:
+            t.append('Fizz')
+        elif i % 5 == 0:
+            t.append('Buzz')
+        else:
+            t.append(i)
+
+    return t
+
+if __name__ == '__main__':
+    assert (get_fizzbuzz_list(15))==[1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz']
