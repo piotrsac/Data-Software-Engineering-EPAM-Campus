657b1cc1c8dbe60d3ad1cedc3954729816a0c9e9
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-23 20:29:11 +0100

    submitting task2 solution

--- 2.Python-Core/data-types-strings-task-2/tasks/task.py
+++ 2.Python-Core/data-types-strings-task-2/tasks/task.py
@@ -2,4 +2,20 @@ def get_longest_word( s: str) -> str:
     """
      Add your code here 
     """
-    return ''
+    """
+    >>> get_longest_word('Python is simple and effective!')
+    'effective!'
+    """
+    max_len = -1
+    res=''
+    for i in s.split():
+        if len(i)>max_len:
+            max_len = len(i)
+            res=i
+    return res
+# print(get_longest_word('Python is simple and effective!'))
+
+if __name__ == '__main__':
+    assert(get_longest_word('Python is simple and effective!') == 'effective!')
+    assert(get_longest_word('Ala ma kota') == 'kota')
+    assert(get_longest_word('Piotrek siusiak') == 'Piotrek')
