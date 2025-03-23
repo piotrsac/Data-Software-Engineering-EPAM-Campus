040ccec0ab125917bdd917fbfe90e470abdd69ae
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-23 21:46:33 +0100

    commiting solution to task 3

--- 2.Python-Core/data-types-strings-task-3/tasks/task.py
+++ 2.Python-Core/data-types-strings-task-3/tasks/task.py
@@ -2,4 +2,15 @@ def replacer(s: str) -> str:
     """
     Add your code here
     """
-    return ''
+    t=''
+    for i in range(len(s)):
+        if s[i]=='\'':
+            t+='\"'
+        elif s[i]=='\"':
+            t+='\''
+        else:
+            t+=s[i]
+    return t
+
+if __name__ == '__main__':
+    assert(replacer('This is my \'Sentence\' where i use \"quotes\"')) == 'This is my \"Sentence\" where i use \'quotes\''
