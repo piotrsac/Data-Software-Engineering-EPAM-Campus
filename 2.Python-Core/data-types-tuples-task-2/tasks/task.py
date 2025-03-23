d7583ea57503a7e840f7f2f4045f67b4c0e05b5a
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-23 23:52:36 +0100

    submitting task2

--- 2.Python-Core/data-types-tuples-task-2/tasks/task.py
+++ 2.Python-Core/data-types-tuples-task-2/tasks/task.py
@@ -2,4 +2,22 @@ from typing import Any, Tuple, List
 
 def get_pairs(lst: List[Any]) -> List[Tuple[Any, Any]]:
     # TODO: Add your code here
-    return []
+    if len(lst) < 2:
+        return []
+    res=[]
+    for i in range(len(lst) - 1):
+        res.append((lst[i], lst[i + 1]))
+
+    return res
+
+if __name__ == '__main__':
+    assert get_pairs([1, 2, 3, 8, 9])==[(1, 2), (2, 3), (3, 8), (8, 9)]
+    assert get_pairs([1])==[]
+    assert get_pairs(['need', 'to', 'sleep', 'more'])==[('need', 'to'), ('to', 'sleep'), ('sleep', 'more')]
+
+""">>> get_pairs([1, 2, 3, 8, 9])
+[(1, 2), (2, 3), (3, 8), (8, 9)]
+>>> get_pairs(['need', 'to', 'sleep', 'more'])
+[('need', 'to'), ('to', 'sleep'), ('sleep', 'more')] 
+>>> get_pairs([1])
+[]"""
