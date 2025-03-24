3f99d0ee2782796d7e6030be39dcaf3620369a94
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-24 10:00:09 +0100

    commiting solution to task 1

--- 2.Python-Core/data-types-dictionaries-task-1/tasks/task.py
+++ 2.Python-Core/data-types-dictionaries-task-1/tasks/task.py
@@ -2,5 +2,17 @@ from typing import Dict
 
 
 def get_dict(s: str) -> Dict[str, int]:
-    # TODO: Add your code here
-    return {}
+    dictionary = {}
+    s=s.lower()
+    for i in s:
+        if i not in dictionary.keys():
+            dictionary[i] = 1
+        else:
+            dictionary[i] += 1
+
+    return dictionary
+
+
+if __name__ == '__main__':
+    # print(get_dict('Oh, it is python'))
+    assert get_dict('Oh, it is python')=={" ": 3, ",": 1, "h": 2, "i": 2, "n": 1, "o": 2, "p": 1, "s": 1, "t": 2, "y": 1}
