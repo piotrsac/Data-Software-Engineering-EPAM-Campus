88d069d7892364e18a869d65d4622be1eb67597c
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-28 17:54:17 +0100

    commiting solution to task2

--- 2.Python-Core/functions-decorators-recursions-task-2/tasks/task.py
+++ 2.Python-Core/functions-decorators-recursions-task-2/tasks/task.py
@@ -4,4 +4,13 @@ def linear_seq(sequence: List[Any]) -> List[Any]:
     """
     Add your code here or call it from here   
     """
-    pass
+    result=[]
+    for item in sequence:
+        if type(item) == int:
+            result.append(item)
+        else:
+            result += (linear_seq(item))
+    return result
+
+if __name__ == '__main__':
+    assert (linear_seq([1,2,3,[4,5,(6,7)]])) == [1,2,3,4,5,6,7],print(linear_seq([1,2,3,[4,5, (6,7)]]))
