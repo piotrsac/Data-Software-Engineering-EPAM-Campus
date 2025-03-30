d12a1c63d00b89366a544fb0dc5886336b4e079d
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-30 23:29:19 +0200

    test version of task 2

--- 2.Python-Core/functions-decorators-decorators-task-2/tasks/task.py
+++ 2.Python-Core/functions-decorators-decorators-task-2/tasks/task.py
@@ -1,6 +1,35 @@
+from time import time, sleep
 
 def log(fn):
     """
     Add your code here or call it from here   
     """
-    pass
+    file=open('log.txt','a')
+    def wrapper(*args, **kwargs):
+        time_start = time()
+        fn(*args, **kwargs)
+        execution_time = time() - time_start
+        # line = [f'{fn.__name__}; args:{args}; kwargs:{kwargs}; execution_time: {execution_time}sec.\n']
+        arg_str = ', '.join(f'{arg}' for arg in args)
+        kwarg_str = ', '.join(f'{key}={value}' for key,value in kwargs.items())
+        file.write(f'{fn.__name__}; args: {arg_str}; kwargs: {kwarg_str}; execution_time: {execution_time} sec.\n')
+    return wrapper
+
+@log
+def foo(a, b, c):
+    sleep(0.2)
+    return a + b + c/a + c/b * 5/a
+
+foo(1, 2, c=3)
+
+"""
+Example of using
+@log
+def foo(a, b, c):
+    ...
+
+foo(1, 2, c=3)
+log.txt
+...
+foo; args: a=1, b=2; kwargs: c=3; execution time: 0.12 sec.
+..."""
