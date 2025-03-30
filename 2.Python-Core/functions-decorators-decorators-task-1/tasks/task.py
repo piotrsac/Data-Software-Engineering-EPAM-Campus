a55c58e3d6477fb42886b8e7ee15ab568ed2aef2
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-30 22:58:31 +0200

    commiting solution to task1

--- 2.Python-Core/functions-decorators-decorators-task-1/tasks/task.py
+++ 2.Python-Core/functions-decorators-decorators-task-1/tasks/task.py
@@ -1,7 +1,8 @@
+from time import time
+from time import sleep
 from typing import Dict
 
 execution_time: Dict[str, float] = {}
-
 
 def time_decorator(fn):
     """
@@ -10,4 +11,19 @@ def time_decorator(fn):
     and put this time value to `execution_time` dictionary where `key` is
     decorated function name and `value` is this function execution time.
     """
-    pass
+    def wrapper(*args, **kwargs):
+        time_start = time()
+        result = fn(*args, **kwargs)
+        execution_time[fn.__name__] = time() - time_start
+        return result
+
+    return wrapper
+
+@time_decorator
+def func_add(a, b):
+    sleep(0.2)
+    return a + b
+
+# print(func_add(1, 2))
+# print(execution_time)
+
