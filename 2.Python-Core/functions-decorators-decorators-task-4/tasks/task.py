64e1ba474e8bf58223d494d7d9bfaedca3a1f381
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-03-31 12:50:56 +0200

    commiting solution to task 4

--- 2.Python-Core/functions-decorators-decorators-task-4/tasks/task.py
+++ 2.Python-Core/functions-decorators-decorators-task-4/tasks/task.py
@@ -1,11 +1,17 @@
 
 def decorator_apply(lambda_func):
-    '''
-    Add your implementation here
-    '''
-    pass
+    def decorator(func):
+        def wrapper(num):
+            return func(lambda_func(num))
+        return wrapper
+    return decorator
 
 
 @decorator_apply(lambda user_id: user_id + 1)
 def return_user_id(num: int) ->int:
     return num
+
+if __name__ == '__main__':
+    assert return_user_id(3) == 4
+    assert return_user_id(-5) == -4
+    assert return_user_id(256) == 257
