3281303cd845701bf95f2f3b25085dffce1315f7
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-03 21:08:58 +0200

    commiting final version of the task1

--- 2.Python-Core/functions-decorators-final-task-1/tasks/task.py
+++ 2.Python-Core/functions-decorators-final-task-1/tasks/task.py
@@ -1,14 +1,19 @@
 from typing import List
 
-def split(data: str, sep=' ', maxsplit=-1):
-    """
-    Add your code here or call it from here   
-    """
-    num_of_splits = maxsplit
+def split(data: str, sep=' ', maxsplit=-1) -> List[str]:
+    '''
+    My custom str.split() function
+    :param data: string
+    :param sep: separator
+    :param maxsplit: maximum number of elements in str.split, if -1 then no limit
+    :return: list of strings splitted by separator
+    '''
     data=data.strip()
-    data_work=data
-    if len(data) == 0:
+    working_data=data
+    if len(data) == 0 and sep == ' ': #weird edge cases, that's how str.split() works
         return []
+    if len(data) == 0:
+        return ['']
     if maxsplit == 0:
         return [data.strip()]
     result=[]
@@ -23,25 +28,33 @@ def split(data: str, sep=' ', maxsplit=-1):
                 if data[i + j] != sep[j]:
                     break
             else:
-                result.append(data_work[last_split:i])
+                if maxsplit == 0:
+                    break
+                result.append(working_data[last_split:i])
                 last_split = i + len(sep)
                 i += len(sep) - 1
+                maxsplit-=1
                 continue
 
-    result.append(data_work[last_split:])
+    result.append(working_data[last_split:])
     return result
 
 if __name__ == '__main__':
     assert split('') == [],print(split(''))
+    assert split('') == ''.split()
+    assert split('',sep=',') == ''.split(sep=','),print(''.split(sep=','))
     assert split('123  123   123 123') == '123  123   123 123'.split(),print(split('123  123   123 123'))
     assert split(',123,', sep=',') == ['', '123', ''],print(split(',123,', sep=','))
     assert split('test') == ['test'],print(split('test'))
-    #assert split('Python    2     3', maxsplit=1) == ['Python', '2     3']
-    #assert split('    test     6    7', maxsplit=1) == ['test', '6    7'],print(split('    test     6    7', maxsplit=1))
+    assert split('Python    2     3', maxsplit=1) == ['Python', '2     3'],print(split('Python    2     3',maxsplit=1))
+    assert split('    test     6    7', maxsplit=1) == ['test', '6    7'],print(split('    test     6    7', maxsplit=1))
     assert split('    Hi     8    9', maxsplit=0) == ['Hi     8    9'],print(split('Hi     8    9',maxsplit=0))
+    assert split(';;;;sp;;;;t;;;;;;;;;;',sep=';') == ';;;;sp;;;;t;;;;;;;;;;'.split(sep=';')
     assert split('    set   3     4') == ['set', '3', '4'],print(split('    set   3     4'))
     assert split('set;:23', sep=';:') == ['set','23'], print(split('set;:23', sep=';:'))
     assert split('set;:;;:;23', sep=';:;') == 'set;:;;:;23'.split(sep=';:;'), print(split('set;:;;:;23', sep=';:;'))
     assert split('set;:;23', sep=';:;') == ['set', '23'], print(split('set;:;;:;23', sep=';:;'))
-    #assert split('set;:23', sep=';:', maxsplit=0) == ['set;:23'],print(split('set;:23', sep=';:', maxsplit=0))
-    #assert split('set;:;:23', sep=';:', maxsplit=2) == ['set', '', '23']
+    assert split(';;;;;;;;;;set;;;;;;;23;;;;;;;;;;;',sep=';') == ';;;;;;;;;;set;;;;;;;23;;;;;;;;;;;'.split(sep=';'),print(';;;;;;;;;;set;;;;;;;23;;;;;;;;;;;'.split(sep=';'))
+    assert split('set;:23', sep=';:', maxsplit=0) == ['set;:23'],print(split('set;:23', sep=';:', maxsplit=0))
+    assert split('set;:;:23', sep=';:', maxsplit=2) == ['set', '', '23']
+
