d29c63ec57bbf7030b625a075b3f2c104862f68d
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-22 19:10:43 +0200

    commiting solution to task 3

--- 2.Python-Core/oop-basics-task-3/tasks/task.py
+++ 2.Python-Core/oop-basics-task-3/tasks/task.py
@@ -1,2 +1,28 @@
 class Counter:
-    pass
+    def __init__(self,start=0,stop=float('inf')):
+        self.start = start
+        self.stop = stop
+    def increment(self):
+        if self.start < self.stop:
+            self.start += 1
+        else:
+            print('Maximal value is reached.')
+    def get(self):
+        print(self.start)
+
+if __name__ == '__main__':
+    c=Counter(start=42)
+    c.increment()
+    c.get()
+
+    c=Counter()
+    c.increment()
+    c.get()
+    c.increment()
+    c.get()
+
+    c=Counter(start=42, stop=43)
+    c.increment()
+    c.get()
+    c.increment()
+    c.get()
