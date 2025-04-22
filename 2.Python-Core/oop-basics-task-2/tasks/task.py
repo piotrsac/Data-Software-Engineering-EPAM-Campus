ee1cef4fc8d7269f0c1726f509b9c66c776689bd
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-22 18:50:00 +0200

    submitting solution to task 2

--- 2.Python-Core/oop-basics-task-2/tasks/task.py
+++ 2.Python-Core/oop-basics-task-2/tasks/task.py
@@ -2,12 +2,25 @@
 # Don't change names of classes. Create names for the variables
 # exactly the same as in the task.
 class SchoolMember:
-    pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+class Teacher(SchoolMember):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+    def show(self):
+        return f'Name: {self.name}, Age: {self.age}, Salary: {self.salary}'
 
-class Teacher:
-    pass
+class Student(SchoolMember):
+    def __init__(self, name, age, grades):
+        super().__init__(name, age)
+        self.grades = grades
+    def show(self):
+        return f'Name: {self.name}, Age: {self.age}, Grades: {self.grades}'
 
-
-class Student:
-    pass
+if __name__ == '__main__':
+    persons = [Teacher("Mr.Snape", 40, 3000), Student("Harry", 16, 75)]
+    for person in persons:
+        print(person.show())
