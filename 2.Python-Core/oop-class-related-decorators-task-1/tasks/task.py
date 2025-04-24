111bd0d6c57a7f547438a2a5a67ff214084f6264
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-24 22:42:25 +0200

    commiting solution to task 1

--- 2.Python-Core/oop-class-related-decorators-task-1/tasks/task.py
+++ 2.Python-Core/oop-class-related-decorators-task-1/tasks/task.py
@@ -1,5 +1,5 @@
 from abc import ABC
-# from abc import abstractmethod
+from abc import abstractmethod
 
 
 class Vehicle(ABC):
@@ -10,19 +10,26 @@ class Vehicle(ABC):
             base_price: int,
             mileage: int
     ):
-        pass
+        self.brand_name = brand_name
+        self.year_of_issue = year_of_issue
+        self.base_price = base_price
+        self.mileage = mileage
 
+    @abstractmethod
     def wheels_num(self) -> int:
         return 0
 
     def vehicle_type(self) -> str:
-        pass
+        return f'{self.brand_name} {self.__class__.__name__}'
 
     def is_motorcycle(self) -> bool:
-        pass
+        return self.wheels_num() == 2
 
+    @property
     def purchase_price(self) -> float:
-        pass
+        if self.base_price - 0.1 * self.mileage < 100000:
+            return 100000
+        return self.base_price - 0.1 * self.mileage
 
 
 # Don't change class implementation
@@ -47,3 +54,18 @@ class Truck(Vehicle):
 class Bus(Vehicle):
     def wheels_num(self):
         return 6
+
+if __name__ == '__main__':
+    vehicles = (
+        Car(brand_name="Toyota", year_of_issue=2020, base_price=1_000_000, mileage=150_000),
+        Motorcycle(brand_name="Suzuki", year_of_issue=2015, base_price=800_000, mileage=35_000),
+        Truck(brand_name="Scania", year_of_issue=2018, base_price=15_000_000, mileage=850_000),
+        Bus(brand_name="MAN", year_of_issue=2000, base_price=10_000_000, mileage=950_000)
+    )
+    for vehicle in vehicles:
+        print(
+            f"Vehicle type={vehicle.vehicle_type()}\n"
+            f"Is motorcycle={vehicle.is_motorcycle()}\n"
+            f"Purchase price={vehicle.purchase_price}\n"
+        )
+
