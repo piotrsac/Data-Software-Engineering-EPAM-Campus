982ae76f06b5eea81c9383dafe0aad8f6f8fcbba
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-27 23:19:17 +0200

    submitting solution to task 1

--- 2.Python-Core/oop-magic-methods-task-1/tasks/task.py
+++ 2.Python-Core/oop-magic-methods-task-1/tasks/task.py
@@ -4,4 +4,10 @@ from typing import List
 class Counter:
     def __init__(self, values: List[int]):
         self.values = values
-    # TODO: add your code here
+
+    def __add__(self, string):
+        return [f'{value} {string}' for value in self.values ]
+
+
+if __name__ == '__main__':
+    assert Counter([1, 2, 3]) + "mississippi" == ["1 mississippi", "2 mississippi" , "3 mississippi"]
