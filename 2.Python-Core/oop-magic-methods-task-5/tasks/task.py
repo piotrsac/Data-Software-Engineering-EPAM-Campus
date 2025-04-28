2c52bca7ad732a6ec1aa8d45ee192915db090618
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-29 01:51:22 +0200

    submitting test solution

--- 2.Python-Core/oop-magic-methods-task-5/tasks/task.py
+++ 2.Python-Core/oop-magic-methods-task-5/tasks/task.py
@@ -1,6 +1,20 @@
 import os
 import shutil
+import tempfile
+from random import randint
 
 class TempDir:
-    pass
-    # TODO: please ad your code here
+    def __init__(self):
+        self.wd_path = None
+        self.temp_dir_path = None
+
+    def __enter__(self):
+        dir_name = randint(0,9999999)
+        os.mkdir(f'{tempfile.gettempdir()}/{dir_name}')
+        self.temp_dir_path = f'{tempfile.gettempdir()}/{dir_name}'
+        self.wd_path = os.getcwd()
+        os.chdir(self.temp_dir_path)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        os.chdir(self.wd_path)
+        shutil.rmtree(self.temp_dir_path)
