c15bb372f98c579dfd5f99192bc3d5ff782062a9
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-28 15:56:23 +0200

    remodeling the code structure, improving quality

--- 2.Python-Core/oop-magic-methods-task-3/tasks/task.py
+++ 2.Python-Core/oop-magic-methods-task-3/tasks/task.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 from typing import Type
 
 
-class Currency:
+class Currency(ABC):
     """
     1 EUR = 2 USD = 100 GBP
 
@@ -10,72 +11,82 @@ class Currency:
     1 USD = 0.5 EUR  ;  1 USD = 50 GBP
     1 GBP = 0.02 USD ;  1 GBP = 0.01 EUR
     """
-    abbr_dict = {"Dollar": "USD", "Euro": "EUR", "Pound": "GBP"}
-    course_dict = {"Euro": 100, "Dollar": 50, "Pound": 1}
+    @property
+    @abstractmethod
+    def abbr(self) -> str:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def conv_rate(self) -> int:
+        raise NotImplementedError
 
     def __init__(self, value: float):
         self.value = value
 
     @classmethod
-    def course(cls, other_cls: Type[Currency]) -> str:
-        return (f"{Currency.course_dict[cls.__name__]/Currency.course_dict[other_cls.__name__]} "
-                f"{Currency.abbr_dict[other_cls.__name__]} "
-                f"for 1 {Currency.abbr_dict[cls.__name__]}")
+    def rate(cls, other_cls: Type[Currency]) -> float:
+        return cls.conv_rate / other_cls.conv_rate
 
-    def to_currency(self, other_cls: Type[Currency]):
-        rate = (Currency.course_dict[self.__class__.__name__] / Currency.course_dict[other_cls.__name__])
-        converted_value = self.value * rate
+    @classmethod
+    def course(cls, other_cls: Type[Currency]) -> str:
+        return (f"{cls.rate(other_cls)} "
+                f"{other_cls.abbr} "
+                f"for 1 {cls.abbr}")
+
+    def to_currency(self, other_cls: Type[Currency]) -> Currency:
+        converted_value = self.value * self.rate(other_cls)
         return other_cls(converted_value)
 
-    def __add__(self, other):
-        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
-        sum_value = self.value + other.value * rate
+    def __str__(self) -> str:
+        return f'{self.value} {self.abbr}'
+
+    def __add__(self, other) -> Currency:
+        sum_value = self.value + other.value * other.rate(self)
         return self.__class__(sum_value)
 
-    def __eq__(self, other):
-        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
-        print(self.value, other.value, rate, other.value * rate)
-        return self.value == other.value * rate
+    def __eq__(self, other) -> bool:
+        return self.value == other.value * other.rate(self)
 
-    def __gt__(self, other):
-        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
-        return self.value > other.value * rate
+    def __gt__(self, other) -> bool:
+        return self.value > other.value * other.rate(self)
 
-    def __lt__(self, other):
-        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
-        return self.value < other.value * rate
+    def __lt__(self, other) -> bool:
+        return self.value < other.value * other.rate(self)
+
 
 class Euro(Currency):
-    def __str__(self):
-        return f'{self.value} EUR'
+    abbr = "EUR"
+    conv_rate = 100
 
 
 class Dollar(Currency):
-    def __str__(self):
-        return f'{self.value} USD'
+    abbr = "USD"
+    conv_rate = 50
 
 
 class Pound(Currency):
-    def __str__(self):
-        return f'{self.value} GBP'
+    abbr = "GBP"
+    conv_rate = 1
 
 
 if __name__ == "__main__":
     e = Euro(100)
     r = Pound(100)
     d = Dollar(200)
-    print(f'e = {e}\n')
-    print(Euro.course(Pound))
-    print(Dollar.course(Pound))
-    print(Pound.course(Euro))
-    print(f"e.to_currency(Dollar) = {e.to_currency(Dollar)}\n")
-    print(f"e.to_currency(Pound) = {e.to_currency(Pound)}\n")
-    print(f"e.to_currency(Euro)   = {e.to_currency(Euro)}\n")
-    print(
-        f"e + r  =>  {e + r}\n"
-        f"r + d  =>  {r + d}\n"
-        f"d + e  =>  {d + e}\n"
-    )
+    assert f'e = {e}' == "e = 100 EUR"
+    assert Euro.course(Pound) == "100.0 GBP for 1 EUR"
+    assert Dollar.course(Pound) == "50.0 GBP for 1 USD"
+    assert Pound.course(Euro) == "0.01 EUR for 1 GBP"
+    assert f'{e.to_currency(Dollar)}' == "200.0 USD"
+    assert f'{e.to_currency(Pound)}' == "10000.0 GBP"
+    assert f'{e.to_currency(Euro)}' == "100.0 EUR"
+    assert f'{e + r}' == "101.0 EUR"
+    assert f'{r + d}' == "10100.0 GBP"
+    assert f'{d + e}' == "400.0 USD"
     usd = Dollar(100)
     eur = Euro(50)
-    print(usd == eur)
+    assert (usd == eur) is True
+    assert (e == r) is False
+    assert (e > r) is True
+    assert (e < r) is False
