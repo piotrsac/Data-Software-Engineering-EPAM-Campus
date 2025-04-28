d46c5949cf3a53a3f111da4451d82e77320f1410
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-28 14:13:35 +0200

    sumbmitting test solution for now, will improve code quality

--- 2.Python-Core/oop-magic-methods-task-3/tasks/task.py
+++ 2.Python-Core/oop-magic-methods-task-3/tasks/task.py
@@ -10,40 +10,52 @@ class Currency:
     1 USD = 0.5 EUR  ;  1 USD = 50 GBP
     1 GBP = 0.02 USD ;  1 GBP = 0.01 EUR
     """
+    abbr_dict = {"Dollar": "USD", "Euro": "EUR", "Pound": "GBP"}
+    course_dict = {"Euro": 100, "Dollar": 50, "Pound": 1}
+
     def __init__(self, value: float):
         self.value = value
 
     @classmethod
     def course(cls, other_cls: Type[Currency]) -> str:
-        abbr = {"Dollar" : "USD", "Euro" : "EUR", "Pound" : "GBP"}
-        course = {"Dollar" : 50, "Euro" : 100, "Pound" : 1}
-        return (f"{course[cls.__name__]/course[other_cls.__name__]} {abbr[other_cls.__name__]} "
-                f"for 1 {abbr[cls.__name__]}")
+        return (f"{Currency.course_dict[cls.__name__]/Currency.course_dict[other_cls.__name__]} "
+                f"{Currency.abbr_dict[other_cls.__name__]} "
+                f"for 1 {Currency.abbr_dict[cls.__name__]}")
 
     def to_currency(self, other_cls: Type[Currency]):
-        raise NotImplementedError
+        rate = (Currency.course_dict[self.__class__.__name__] / Currency.course_dict[other_cls.__name__])
+        converted_value = self.value * rate
+        return other_cls(converted_value)
 
+    def __add__(self, other):
+        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
+        sum_value = self.value + other.value * rate
+        return self.__class__(sum_value)
+
+    def __eq__(self, other):
+        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
+        print(self.value, other.value, rate, other.value * rate)
+        return self.value == other.value * rate
+
+    def __gt__(self, other):
+        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
+        return self.value > other.value * rate
+
+    def __lt__(self, other):
+        rate = (Currency.course_dict[other.__class__.__name__] / Currency.course_dict[self.__class__.__name__])
+        return self.value < other.value * rate
 
 class Euro(Currency):
-    def __init__(self, value: float):
-        super().__init__(value)
-
     def __str__(self):
         return f'{self.value} EUR'
 
 
 class Dollar(Currency):
-    def __init__(self, value: float):
-        super().__init__(value)
-
     def __str__(self):
         return f'{self.value} USD'
 
 
 class Pound(Currency):
-    def __init__(self, value: float):
-        super().__init__(value)
-
     def __str__(self):
         return f'{self.value} GBP'
 
@@ -56,3 +68,14 @@ if __name__ == "__main__":
     print(Euro.course(Pound))
     print(Dollar.course(Pound))
     print(Pound.course(Euro))
+    print(f"e.to_currency(Dollar) = {e.to_currency(Dollar)}\n")
+    print(f"e.to_currency(Pound) = {e.to_currency(Pound)}\n")
+    print(f"e.to_currency(Euro)   = {e.to_currency(Euro)}\n")
+    print(
+        f"e + r  =>  {e + r}\n"
+        f"r + d  =>  {r + d}\n"
+        f"d + e  =>  {d + e}\n"
+    )
+    usd = Dollar(100)
+    eur = Euro(50)
+    print(usd == eur)
