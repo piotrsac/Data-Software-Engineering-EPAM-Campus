f6d78e860335a4b2352ddc21d2b51bb1e677d08a
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-29 17:45:21 +0200

    testing solution to final task 1

--- 2.Python-Core/oop-final-tasks-task-1/tasks/task.py
+++ 2.Python-Core/oop-final-tasks-task-1/tasks/task.py
@@ -1,3 +1,18 @@
 class Sun:
-    pass
-    # TODO: please add your code here
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if Sun._instance is not None:
+            raise RuntimeError("Use 'Sun.inst()' to get the instance")
+
+    @classmethod
+    def inst(cls):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
+
+if __name__ == '__main__':
+    p = Sun.inst()
+    f = Sun.inst()
+    assert (p is f) is True
