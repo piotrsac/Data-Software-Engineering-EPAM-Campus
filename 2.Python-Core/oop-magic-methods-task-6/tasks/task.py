01940bce5fff80153a7d731b9d69e115cb28b8d9
Piotr Sączawa <piotrsaczawa3@gmail.com>
2025-04-29 11:35:33 +0200

    submitting solution to task 6

--- 2.Python-Core/oop-magic-methods-task-6/tasks/task.py
+++ 2.Python-Core/oop-magic-methods-task-6/tasks/task.py
@@ -2,5 +2,15 @@ import os
 
 
 class Cd:
-    pass
-    # TODO: please add your code here
+    def __init__(self, pointed_directory):
+        self.originalPath = None
+        if not os.path.isdir(pointed_directory):
+            raise ValueError
+        self.pointed_directory = pointed_directory
+
+    def __enter__(self):
+        self.originalPath = os.getcwd()
+        os.chdir(self.pointed_directory)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        os.chdir(self.originalPath)
